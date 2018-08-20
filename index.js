9e41d21e2ef12352af08eef3f2ec5d31a01c27ca
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-20 23:36:28 +0000

    made request for css

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
 var server = http.createServer(function(request, response){
+    console.log(`${request.method} request for ${request.url}`);
+    
     var page;
     if (request.url === '/') {
         page = 'home';
-    } else if (request.url === '/contact') {
-        page = 'contact';
-    } else if (request.url === '/about') {
-        page = 'about';
-    } else {
-        page = '404 page not found'
-    }
-
-    response.writeHead(200, {'Content-Type':'text/html'});
-    response.end(`
-        <html>
-            <head>
-                <title>Node Server</title>
-            </head>
-            <body>
-                <h1>${page}</h1>
-                <p>${request.url}</p>
-                <p>${request.method}</p>
-            </body>
-        </html>
-        
-        `);
-    });
-    
+        fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log(error);
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url.match(/.css$/) ){
+        var cssPath = path.join(__dirname, 'public', request.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        fileStream.pipe(response);
+    };
+});
 server.listen(3000);
 
 console.log('The server is running on port 3000');
+    // else if (request.url === '/contact') {
+    //     page = 'contact';
+    // } else if (request.url === '/about') {
+    //     page = 'about';
+    // } else {
+    //     page = '404 page not found'
+    // }
+
+    // response.writeHead(200, {'Content-Type':'text/html'});
+    // response.end(`
+    //     <html>
+    //         <head>
+    //             <title>Node Server</title>
+    //         </head>
+    //         <body>
+    //             <h1>${page}</h1>
+    //             <p>${request.url}</p>
+    //             <p>${request.method}</p>
+    //         </body>
+    //     </html>
+        
+    //     `);
+    
     
     
     
