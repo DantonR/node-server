37751cda2d665197de2893624b85d0486882c2c8
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-20 21:55:13 +0000

    added first routes section for our nodejs app

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const http = require('http');
 var server = http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.end('This is running from a server, changed again');
-});
+    var page;
+    if (request.url === '/') {
+        page = 'home';
+    } else if (request.url === '/contact') {
+        page = 'contact';
+    } else if (request.url === '/about') {
+        page = 'about';
+    } else {
+        page = '404 page not found'
+    }
+
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end(`
+        <html>
+            <head>
+                <title>Node Server</title>
+            </head>
+            <body>
+                <h1>${page}</h1>
+                <p>${request.url}</p>
+                <p>${request.method}</p>
+            </body>
+        </html>
+        
+        `);
+    });
+    
 server.listen(3000);
 
 console.log('The server is running on port 3000');
+    
+    
+    
 
